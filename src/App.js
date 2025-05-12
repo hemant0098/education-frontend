838ac76503b3e6d57a8328e42972440f0470b431
Hemant <hemantdevgan.0098@gmail.com>
2025-05-12 16:54:03 +0530

    new one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Navbar from './components/navbar'
 import Footer from './components/footer'
